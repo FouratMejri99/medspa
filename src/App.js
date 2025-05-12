b5ca04ec3fe1abc1a7a486e00d36f39ee38cdbbe
Fourat Mejri <32815591+fouratMejri@users.noreply.github.com>
2025-05-12 20:19:58 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import React from "react";
 import AboutSection from "./aboutsection/about";
 import "./App.css";
 import Footer from "./footer/footer";
@@ -9,35 +8,36 @@ import ServiceSection from "./Servicessection/services";
 function App() {
   return (
     <div className="App">
-      {/* Background Video */}
-      <div className="video-container">
-        <video autoPlay loop muted playsInline className="background-clip">
-          <source src="/bellevideo.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-
-      {/* Welcome Section */}
-      <section id="welcome" className="welcome-section">
-        <div className="welcome-content">
-          <h1 className="welcome-title">Welcome to</h1>
-          <h2 className="welcome-subtitle">B E L L E</h2>
-
-          <h3 className="welcome-slogan">Beauty from the inside out</h3>
-        </div>
-      </section>
-
       {/* Header */}
       <Header />
+      {/* Background Video */}
+      <main className="main-content">
+        <div className="video-container">
+          <video autoPlay loop muted playsInline className="background-clip">
+            <source src="/bellevideo.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
 
-      {/* Main Content */}
-      <div className="content">
-        <AboutSection />
-        <ServiceSection />
-      </div>
+        {/* Welcome Section */}
+        <section id="welcome" className="welcome-section">
+          <div className="welcome-content">
+            <h1 className="welcome-title">Welcome to</h1>
+            <h2 className="welcome-subtitle">B E L L E</h2>
 
-      {/* Footer */}
-      <Footer />
+            <h3 className="welcome-slogan">Beauty from the inside out</h3>
+          </div>
+        </section>
+
+        {/* Main Content */}
+        <div className="content">
+          <AboutSection />
+          <ServiceSection />
+        </div>
+
+        {/* Footer */}
+        <Footer />
+      </main>
     </div>
   );
 }
